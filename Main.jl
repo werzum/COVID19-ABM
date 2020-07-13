3966408dfddc84e7ebe09ca3ab6ae449e2c9cc15
Carl Orge Retzlaff <e.awerg@web.de>
2020-07-13 23:17:37 +0200

    Basic stepping function works.Now: Troubleshooting and better parameters

--- Main.jl
+++ Main.jl
@@ -3,6 +3,8 @@ using CSV
 using Plots
 
 #TODO add arrays to keep track of the schools, homes, workplaces, so that we can set custom infection rates and so forth for them.
+#TODO map is unweighted so far, could add wheights but then have to
+#TODO could clear the warnings about changed uses of filter, csv read and filter
 
 include("SpatialSetup.jl") #exports setup
 #include("agent_functions.jl") #exports agent_step
@@ -13,6 +15,7 @@ include("Visualization.jl")# exports draw_route(model,lat,long) and draw_map(mod
 mutable struct DemoAgent <: AbstractAgent
     id::Int
     pos::Int
+    health_status::Symbol #reflects the SIR extended states (Susceptible, Exposed, Infected, InfectedWitoutSymptpms, NotQuarantined, Quarantined, Dead, Immune)
     women::Bool
     age::Int8
     wealth::Int16
@@ -24,17 +27,18 @@ mutable struct DemoAgent <: AbstractAgent
     socialroute::Vector{LightGraphs.SimpleGraphs.SimpleEdge{Int64}}
     distantroute::Vector{LightGraphs.SimpleGraphs.SimpleEdge{Int64}}
 end
-
-#initialize the model and generate the map
-@time model,lat,long,social_groups,distant_groups = setup()
-
-params = Dict(
+parameters = Dict(
             :beta_det=> 1,
             :beta_undet=> 3,
             :infection_period=> 10,
             :reinfection_probability=> 0.01,
             :detection_time=> 6,
             :death_rate=> 0.02)
+
+#initialize the model and generate the map - takes about 115s
+@time model,lat,long,social_groups,distant_groups = setup(parameters)
+
+#step the model X times - each step takes about Xs
 
 #Plot map
 draw_map(model,lat,long)
--- SteppingFunction.jl
+++ SteppingFunction.jl
@@ -2,102 +2,133 @@ function agent_week!(model, social_groups, distant_groups)
     for i in 1:5
         social_active_group = rand(social_groups,Int.(round.(length(social_groups)/10)))
         distant_active_group = rand(distant_groups,Int.(round.(length(distant_groups)/10)))
-        agent_day(model, day, social_active_group,distant_active_group)
+        infected_edges = Vector{Int32}(undef,0)
+        println("executing agent day")
+        agent_day!(model, social_active_group, distant_active_group)
     end
     for i in 1:2
         social_active_group = rand(social_groups,Int.(round.(length(social_groups)/3)))
         distant_active_group = rand(distant_groups,Int.(round.(length(distant_groups)/3)))
-        step!(model, agent_step!)
+        infected_edges = Vector{Int32}(undef,0)
+        agent_day!(model, social_active_group, distant_active_group)
     end
 end
 
-function agent_day!(model,day)
-    #to work
-    step!(model, agent_step!)
-    #back home
-    step!(model, agent_step!)
-    #if social/distant
-    step!(model, agent_step!)
-    #and back home
-    step!(model, agent_step!)
-end
+function agent_day!(model, social_active_group, distant_active_group)
 
-function agent_step!(agent, model)
-    move!(agent, model)
-    transmit!(agent,model)
-    update!(agent,model)
-    recover_or_die!(agent,model)
-end
+    #TODO add difference between work and social activites
 
-function move!(agent, model)
-    if agent.workplace != 0
-        move_agent!(agent,agent.workplace,model)
-
-    #if he wants to move
-    if rand()<0.005
-        #get random coordinates
-        dims = model.space.dimensions
-        randx = rand(1:1:dims[1])
-        randy = rand(1:1:dims[2])
-        while length(get_node_contents((randx,randy), model))==0
-            randx = rand(1:1:dims[1])
-            randy = rand(1:1:dims[2])
+    #put functions within parent scope so we can read from this scope
+    function move_step!(agent, model)
+        #if agent is infected, add edges on his way to the array of infected edges
+        if (agent.health_status == :I)
+            push!(infected_edges, collect(dst.(ag_route)))
+            push!(infected_edges, src(ag_route[1]))
         end
-        #and move the agent to a none-empty place
-        if length(get_node_contents((randx,randy), model))>1
-            move_agent!(agent,(randx,randy), model)
+        #then go to workplace, social group, or home depending on the time of day
+        if time_of_day == :work && agent.workplace != 0
+            move_agent!(agent,agent.workplace,model)
+        elseif time_of_day == :social && in(agent.socialgroup, social_active_group)
+            move_agent!(agent,agent.socialgroup,model)
+        else
+            move_agent!(agent,agent.household,model)
         end
     end
-end
 
-function transmit!(agent, model)
-    #cant transmit if healthy/recovered
-    agent.status == :S && return
-    agent.status == :R && return
-    prop = model.properties
-
-    #set the detected/undetected infection rate, also check if he doesnt show symptoms
-    rate = if agent.days_infected >= prop[:detection_time] && rand()<=0.8
-            prop[:beta_det]
-    else
-        prop[:beta_undet]
+    function infect_step!(agent, model)
+        transmit!(agent,model)
+        update!(agent,model)
+        #add somewhere the possibility to become infected by travelling the same edges - not exactly transmit, maybe rather update
+        recover_or_die!(agent,model)
     end
 
-    d = Poisson(rate)
-    n = rand(d) #determine number of people to infect, based on the rate
-    n == 0 && return #skip if probability of infection =0
-    timeout = n*2
-    t = 0
-    #infect the number of contacts and then return
-    #node_contents = get_node_contents(agent, model)
-    neighbors = node_neighbors(agent, model)
+    function transmit!(agent, model)
+        #skip transmission for non-sick agents
+        !in(agent.health_status, (:E,:I,:IwS,:NQ)) && return
+        prop = model.properties
 
-    #trying to infect n others from random neighbor node, timeout if in a node without
-    while n > 0 && t < timeout
-        node = rand(neighbors)
-        contents = get_node_contents(node, model)
-        if length(contents)>1
-            infected = id2agent(rand(contents), model)
-            if infected.status == :S || (infected.status == :R && rand() <= prop[:reinfection_probability])
-                infected.status = :I
+        #set the detected/undetected infection rate
+        rate = if in(agent.health_status,(:E,:I,:IWS,:NQ))
+                prop[:beta_det]
+        else
+            prop[:beta_undet]
+        end
+
+        #draw a random number of people to infect in this node
+        d = Poisson(rate)
+        n = rand(d) #determine number of people to infect, based on the rate
+        n == 0 && return #skip if probability of infection =0
+        timeout = n*2
+        t = 0
+
+        #infect the number of contacts and then return
+        #get node of agent, skip if only him
+        node_contents = get_node_contents(agent.pos, model)
+        length(node_contents)==1 && return
+
+        #trying to infect n others in this node, timeout if in a node without eligible neighbors
+        while n > 0 && t < timeout
+            target = id2agent(rand(node_contents), model)
+            #TODO add the product of individual protection and and target protection - keeping it real simple for now
+            if infected.health_status == :S
+                infected.health_status = :I
                 n -= 1
             end
-        end
-        t +=1
-    end
-end
-
-update!(agent, model) = agent.status == :I && (agent.days_infected +=1)
-
-function recover_or_die!(agent, model)
-    if agent.days_infected >= model.properties[:infection_period]
-        if rand() <= model.properties[:death_rate]
-            kill_agent!(agent, model)
-        else
-            agent.status = :R
-            agent.days_infected = 0
+            t +=1
         end
     end
+
+    #increase infection time
+    update!(agent, model) = in(agent.health_status, (:E,:I,:IWS,:NQ,:Q,:M)) && (agent.days_infected +=1)
+
+    #transition agent to new state
+    function recover_or_die!(agent, model)
+        if agent.health_status == :E && agent.days_infected >= 3 #here should go model.properties[:exposed_period]
+            #TODO now 50 50 chance, let age influence this
+            if rand()>0.5
+                agent.health_status = :I
+            else
+                agent.health_status = :IwS
+            end
+        elseif agent.health_status == :I && agent.days_infected >= 5
+            if rand()>0.5
+                agent.health_status = :NQ
+            else
+                agent.health_status = :Q
+            end
+        elseif in(agent.health_status, (:NQ,:Q)) && agent.days_infected >= 10
+            if rand() <= model.properties[:death_rate]
+                kill_agent!(agent, model)
+            else
+                agent.health_status = :M
+                agent.days_infected = 0
+            end
+        elseif agent.health_status == :M && agent.days_infected > 30 #become susceptible again after two weeks
+            agent.health_status = :S
+        end
+    end
+
+    #to work
+    time_of_day = :work
+    println("move to work")
+    step!(model, move_step!)
+    step!(model, infect_step!)
+
+    println("move to back")
+    #back home
+    time_of_day = :back
+    step!(model, move_step!)
+    step!(model, infect_step!)
+
+    #if social/distant
+    time_of_day = :social
+    step!(model, move_step!)
+    step!(model, infect_step!)
+
+    #and back home
+    time_of_day = :back
+    step!(model, move_step!)
+    step!(model, infect_step!)
 end
 
 export agent_step!
