eb12cd840a1f0e9611ddd41524f0b21c9a7ca497
cr480648 <cr480648@login18-1.hpc.itc.rwth-aachen.de>
2020-08-26 11:46:12 +0200

    Adjusted settings live on cluster

--- jobfile.sh
+++ jobfile.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#SBATCH --nodes=2
-#SBATCH --ntasks=4
-#SBATCH --ntasks-per-node=2
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --ntasks-per-node=8
 #SBATCH --time=00:10:00
 #SBATCH --output=myoutput.log
 #SBATCH --job-name=julia_abm_testrun_1
 
-source $HOME/julia-v1.5.0/julia-environment
-cd abm_folder
-julia main.jl
+source $HOME/julia-1.5.0/julia-environment/julia-environment
+cd ~/Julia
+~/julia-1.5.0/bin/julia Main.jl
